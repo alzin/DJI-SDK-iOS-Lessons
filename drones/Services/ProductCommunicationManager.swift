6fbe690fb00de317d8645835ebfbe037cd38838a
Alzin Mohamad Ghaith <35889862+alzin@users.noreply.github.com>
2022-03-20 09:25:22 +0900

    Edit to the whole screen FPV

--- drones/Services/ProductCommunicationManager.swift
+++ drones/Services/ProductCommunicationManager.swift
@@ -9,15 +9,17 @@ import DJISDK
 
 class ProductCommunicationManager: NSObject, DJISDKManagerDelegate {
     
-    let useBridge = true
+    let useBridge = false
     let debugId = "192.168.1.122"
     
+    #warning("Do not forget to change the DJI key here and in your .plist file")
     func registerApp() {
         guard
             let path = Bundle.main.path(forResource: "Info", ofType: "plist"),
             let dict = NSDictionary(contentsOfFile: path) as? Dictionary<String, AnyObject>,
             let appKey = dict["DJISDKAppKey"] as? String,
-            appKey == "839137884bfe4974e4490b40"
+            appKey == "67b2f4f07fbbe62ad280c74b"
+            
         else {
             print("\n<<<ERROR: Please add DJI App Key in Info.plist after registering as a developer>>>\n")
             return
